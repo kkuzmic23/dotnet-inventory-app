828306a5ee96505d61457b154e0bae879a94e117
Karlo Kuzmić <kkuzmic23@foi.hr>
2025-11-22 13:52:51 +0100

    Stvoren UI za Login formu. Stvorena klasa User.

---
+++ Software/SIR/SIR/Class/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SIR.Class
+{
+    internal class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string firstName, string lastName, string username, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Username = username;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
---
+++ Software/SIR/SIR/Login.cs
@@ -0,0 +1,55 @@
+﻿using SIR.Class;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SIR
+{
+    public partial class frmLogin : Form
+    {
+        User kkuzmic;
+        User tlevanic;
+        User tracic;
+        List<User> users;
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+            kkuzmic = new User("Karlo", "Kuzmic", "kkuzmic", "pw123");
+            tlevanic = new User("Tibor", "Levanic", "tlevanic", "pw123");
+            tracic = new User("Tin", "Racic", "tracic", "pw123");
+            users = new List<User>();
+            users.Add(kkuzmic); users.Add(tlevanic); users.Add(tracic);
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text != "" && txtPassword.Text != "")
+            {
+                User found = users.FirstOrDefault(user => user.Username == txtUsername.Text && user.Password == txtPassword.Text);
+
+                if (found != null)
+                {
+                    MessageBox.Show("Login successful!");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login info.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter username and password.");
+            }
+        }
+    }
+}
