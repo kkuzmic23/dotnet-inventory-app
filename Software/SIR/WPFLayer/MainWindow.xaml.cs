1273b7a3f49e95b9e602771ad3bf2a886c42378c
tlevanic23 <tlevanic23@student.foi.hr>
2026-01-31 17:08:20 +0100

    Dodan StackPanel za navigaciju s gumbovima funkcionalnosti, dodan ContentControl element za dinamičko učitavanje sadržaja

--- Software/SIR/WPFLayer/MainWindow.xaml.cs
+++ Software/SIR/WPFLayer/MainWindow.xaml.cs
@@ -24,5 +24,60 @@ namespace WPFLayer
         {
             InitializeComponent();
         }
+
+        private void btnOrders_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnProducts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnSuppliers_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnImport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStock_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExport_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAlerts_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnStatistics_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnReports_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
