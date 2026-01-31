e9ba834545e9823785e5c6d455e9e0e01bc3abf4
Karlo Kuzmić <kkuzmic23@foi.hr>
2026-01-31 13:54:05 +0100

    Napisana metoda za dohvaćanje korisnika po upisanom username-u.

--- Software/SIR/BusinessLogicLayer/UserService.cs
+++ Software/SIR/BusinessLogicLayer/UserService.cs
@@ -4,9 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using DataAccessLayer;
+using EntityLayer.Entities;
+
 namespace BusinessLogicLayer
 {
-    internal class UserService
+    public class UserService
     {
+        private readonly UserRepository _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public User Authenticate(string username, string password)
+        {
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                return null;
+            }
+
+            var user = _userRepository.GetByUsername(username);
+            if (user == null)
+            {
+                return null;
+            }
+
+            if (PasswordHasher.IsHashFormat(user.Password))
+            {
+                return PasswordHasher.Verify(password, user.Password) ? user : null;
+            }
+
+            if (user.Password == password)
+            {
+                var hashed = PasswordHasher.HashPassword(password);
+                _userRepository.UpdatePasswordHash(user.Id, hashed);
+                user.Password = hashed;
+                return user;
+            }
+
+            return null;
+        }
     }
 }
