1340849f788ecd85ff9413f9977e975a58929d98
Karlo Kuzmić <kkuzmic23@foi.hr>
2026-06-12 11:54:48 +0200

    Dodan OrderItemTests.

---
+++ Software/SIR/xUnitTests/OrderItemTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitTests
+{
+    public class OrderItemTests
+    {
+        [Fact]
+
+        public void ValidateOrderItems_ValidOrderItems_ReturnsSuccess()
+        {
+
+        }
+
+        [Theory]
+        [InlineData()]
+        [InlineData()]
+        [InlineData()]
+        public void ValidateOrderItems_MissingRequiredFields_ReturnsFailure()
+        {
+
+        }
+    }
+}
