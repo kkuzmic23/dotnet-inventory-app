6d02313dd32d8d517aefbce4bbf24b708de99b93
Tibor Levanić <Tibor Levani@DESKTOP-25J3OAA>
2026-06-14 15:40:31 +0200

    Napisani dodatni testovi za proizvode s cijenom preko 500 i za negativnu cijenu

--- Software/SIR/xUnitTests/TestDrivenDevelopment/DiscountServiceTests.cs
+++ Software/SIR/xUnitTests/TestDrivenDevelopment/DiscountServiceTests.cs
@@ -19,5 +19,30 @@ namespace xUnitTests.TestDrivenDevelopment
             // Assert
             Assert.Equal(expectedPrice, actualPrice);
         }
+
+        [Fact]
+        public void CalculatePrice_WithAmountOverFiveHundred_ReturnsTwentyPercentDiscount()
+        {
+            // Arrange
+            var service = new DiscountService();
+            double originalPrice = 600.0;
+            double expectedPrice = 480.0; 
+
+            // Act
+            double actualPrice = service.CalculatePrice(originalPrice);
+
+            // Assert
+            Assert.Equal(expectedPrice, actualPrice);
+        }
+
+        [Fact]
+        public void CalculatePrice_WithNegativeAmount_ThrowsArgumentException()
+        {
+            // Arrange
+            var service = new DiscountService();
+
+            // Act & Assert
+            Assert.Throws<System.ArgumentException>(() => service.CalculatePrice(-10.0));
+        }
     }
 }
