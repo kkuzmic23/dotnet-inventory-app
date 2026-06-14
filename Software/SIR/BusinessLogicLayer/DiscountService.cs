8234231f0258fff6cc1e27f9f2933d1beae43ba3
Tibor Levanić <Tibor Levani@DESKTOP-25J3OAA>
2026-06-14 15:43:40 +0200

    Proširena funkcionalnost metode CalculatePrice

--- Software/SIR/BusinessLogicLayer/DiscountService.cs
+++ Software/SIR/BusinessLogicLayer/DiscountService.cs
@@ -4,7 +4,19 @@ namespace BusinessLogicLayer
     {
         public double CalculatePrice(double originalPrice)
         {
-            return originalPrice * 0.9; 
+            if (originalPrice < 0)
+            {
+                throw new System.ArgumentException("Price cannot be negative.");
+            }
+
+            double discount = 0.1; 
+
+            if (originalPrice > 500)
+            {
+                discount = 0.2;
+            }
+
+            return originalPrice * (1 - discount);
         }
     }
 }
